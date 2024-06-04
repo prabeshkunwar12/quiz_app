f7745ab08e449fb34c68afeba40e057a7d4450bc
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-06-04 19:15:59 -0300

    shuffle answers before display

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
   final List<String> answers;
 
   const QuizQuestion({required this.text, required this.answers});
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -20,22 +20,28 @@ class _QuestionScreenState extends State<QuestionScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(text: answer, select: select);
-          })
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(text: answer, select: select);
+            })
+          ],
+        ),
       ),
     );
   }
